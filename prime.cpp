e208964a8a6559e822bd744625e2e9f8608b80bd
Ujwal Sirisala <ujwalsirisala@gmail.com>
2023-09-30 21:53:32 +0530

    commit1

---
+++ prime.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+                  int n;
+                  cout << n;
+                  int i = 2;
+                  while (i < n)
+                  {
+                                    if (n % i == 0)
+                                    {
+                                                      cout << "not prime for" << i << endl;
+                                    }
+                                    else
+                                    {
+                                                      cout << "it is prime" << endl;
+                                    }
+                                    i = i + 1;
+                  }
+}
