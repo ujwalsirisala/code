feb5ef46af8ccd9ee2473ae81ac58ef838534a10
Ujwal Sirisala <ujwalsirisala@gmail.com>
2023-11-07 06:14:16 +0530

    operators

---
+++ arithmaticoperations.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+                  int a;
+                  cout << "enter the value of a" << endl;
+                  cin >> a;
+                  int b;
+                  cout << "enter the value of b" << endl;
+                  cin >> b;
+                  cout << "a+b" << a + b << endl;
+                  cout << "a-b" << a - b << endl;
+                  cout << "a%b" << a % b << endl;
+                  cout << "a/b" << a / b << endl;
+}
